8e395940936619988e660afa7c0c5efa067dac7e
Giovanni Vitiello <giovanni.vitiello10@gmail.com>
2020-09-23 16:32:48 +0200

    Added yellow package. And name of app id appears in textView

--- app/src/main/java/com/example/multiplemanifest/MainActivity.kt
+++ app/src/main/java/com/example/multiplemanifest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.multiplemanifest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appId: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appId = findViewById(R.id.textApplicationId)
+
+        appId.text = BuildConfig.APPLICATION_ID
+
     }
 }
